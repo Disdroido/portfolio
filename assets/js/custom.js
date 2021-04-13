2221220c26424ccb5b2f8183246fb5f77e0b8d5c
Disdroido <disdroido@gmail.com>
2021-04-13 16:10:53 +1000

    updates to about section on the index page and a fade in once it comes into view

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -19,3 +19,26 @@ $("#open-btn").click(function(){
 $("#close-btn").click(function(){
     $("#body").removeClass("overflow-none");
 });
+
+$(function() {
+    function checkElementLocation() {
+        var $window = $(window);
+        var bottom_of_window = $window.scrollTop() + $window.height();
+    
+        $('.fade-elem').each(function(i) {
+        var $that = $(this);
+        var bottom_of_object = $that.position().top + $that.outerHeight();
+    
+        //if element is in viewport, add the animate class
+        if (bottom_of_window > bottom_of_object) {
+            $(this).addClass('fade-in');
+        }
+        });
+    }
+    // if in viewport, show the animation
+    checkElementLocation();
+    
+    $(window).on('scroll', function() {
+        checkElementLocation();
+    });
+});
